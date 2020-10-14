eaa1ad15f72d69f32c2fff35ea77108f0739f8ae
layaramchandra <layaramchandra9@gmail.com>
2020-10-14 19:24:49 +0530

    Revert "Practicing git revert"
    This reverts commit c3017128b0ec79559be30e366912cd860762ddb5.

--- DBUtils.java
+++ DBUtils.java
@@ -1,3 +1,3 @@
 Hi there
 i am practicing git commands
-This line should be removed as part of git revert
+
--- NewFile.java
+++
@@ -1,2 +0,0 @@
-I am new file
-this file should be completely deleted as part of git revert
